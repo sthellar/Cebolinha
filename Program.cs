1b4b8e3af355e563cc03a5622411ff7c41015854
Esther Silva <141463785+sthellar@users.noreply.github.com>
2023-11-20 01:54:56 -0300

    Adicionar a solução da tarefa Cebolinha.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-templ ate for more information
+Console.WriteLine("Seja bem vindo usuário!");
+Console.Write("Digite uma frase: ");
+
+string Frase = Console.ReadLine()!;
+string NovaFrase = Frase.Replace("r", "l");  
+
+Console.WriteLine(NovaFrase);
